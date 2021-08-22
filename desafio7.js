987c51ad3f07f4da837d94c3bc9b237ae06fc591
Apenasgabs <gabers357@gmail.com>
2021-08-22 20:50:42 -0300

    Desafio 7 concluido 😁

---
+++ desafio7.js
@@ -0,0 +1,7 @@
+function tamanhoSetorBusca(areaMaior, areaMenor) {
+    const pedacoMenor = areaMenor / 8;
+    const pedacoMaior = areaMaior / 8;
+    return console.log(pedacoMaior - pedacoMenor);
+}
+
+tamanhoSetorBusca(2834, 314);
