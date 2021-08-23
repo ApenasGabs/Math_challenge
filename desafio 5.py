fa13fecd2d706acb7f5369253d70d328a2f457dc
Apenasgabs <gabers357@gmail.com>
2021-08-22 21:42:27 -0300

    Desafio 5 concluido na gambeta🤣

--- desafio 5.py
+++ desafio 5.py
@@ -1,18 +1,41 @@
-tres_locais = 30 #tres_locais
-yoshis_t = 450 #Yoshi's Island.yoshis_choco
-choco_t = 330 #Chocolate Island. choco_
-cookie_t = 340 #Cookie Mountain. cookie
-yoshis_choco_t =200 #Yoshi's Island e Chocolate Island.
-yoshis_cookie_t = 180 #Yoshi's Island e Cookie Mountain.
-choco_cookie_t = 100 #Chocolate Island e Cookie Mountain.
-maxx = 1000
-yoshis_choco = yoshis_choco_t - tres_locais
-yoshis_cookie = yoshis_cookie_t - tres_locais
-choco_cookie = choco_cookie_t - tres_locais
-yoshis = yoshis_t -yoshis_choco -yoshis_cookie - tres_locais
-choco = choco_t -yoshis_choco -choco_cookie - tres_locais
-cookie = cookie_t -choco_cookie -yoshis_cookie - tres_locais
-um_lugar = yoshis + choco + cookie + yoshis_choco
-nennhum_lugar = maxx - (um_lugar + tres_locais + yoshis_choco + yoshis_cookie + choco_cookie) 
-print(um_lugar,'gostam apenas de uma única localidade e',nennhum_lugar,'não gostam de nenhuma destas localidades')
+import math
 
+
+def gera_frequencia_nota(semitons):
+    # Separação das notas crescentes e decrescentes em listas.
+    notas_crescentes = ['C', 'C#', 'D', 'D#',
+                        'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+    notas_decrescentes = ['C', 'Db', 'D', 'Eb','E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
+    # Estrutura para a repetição da lista em caso da variável 'semitons' ser maior do que a quantidade de itens na lista 'notas_crescentes'.
+    if semitons >= 3:
+        notas = notas_crescentes * math.ceil(semitons / 12)
+        # Correção da variável 'semitons' para posteriormente ser usada como índice dos itens na lista 'notas'.
+        notas_index = (semitons + 9) - 12
+
+    # Estrutura para a repetição da lista em caso da variável 'semitons' ser menor do que o índice mínimo na lista 'notas_decrescentes'.
+    elif semitons <= -10:
+        notas = notas_decrescentes * math.ceil(abs(semitons) / 12)
+        # Correção da variável 'semitons' para posteriormente ser usada como índice dos itens na lista 'notas'.
+        notas_index = (semitons + 9) + 12
+    # Estrutura padrão que atribuirá à variável 'notas', a lista correspondente à condição de negativo ou positivo da variável 'semitons'.
+    else:
+        if semitons >= 0:
+            notas = notas_crescentes
+        else:
+            notas = notas_decrescentes
+        # Correção da variável 'semitons' para posteriormente ser usada como índice dos itens na lista 'notas'.
+        notas_index = (semitons + 9)
+    # Cálculo da frequência da nota (usando a nota 'A' como princípio), a partir do semitom digitado pelo usuário.
+    frequencia = 440*2 ** (semitons/12)
+
+    # Estrutura para verificar se a variável 'frequencia' é um inteiro, para atribuir ao mesmo a característica de string. Se não for, irá ser formatado para string e exibindo 4 casas decimais.
+    if (frequencia).is_integer() == True:
+        frequencia = str(int(frequencia))
+    else:
+        frequencia = format(frequencia, '.4f')
+
+    # Criação da lista final na variável 'resultado'.
+    resultado = [frequencia, notas[notas_index]]
+
+    # Retorno da variável resultado.
+    return resultado
