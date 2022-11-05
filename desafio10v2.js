f1b58de6d0e0c4e47cfe6b051db7557853d84a58
Gabriel Rodrigues <gsantos3@apple.com>
2022-11-05 16:08:31 -0300

    um amigo meu tankou

---
+++ desafio10v2.js
@@ -0,0 +1,37 @@
+function shuffleMusicas(musicasTocadas) {
+  const arr=musicasTocadas
+  const Decrescente=[]
+  const embaralhado=[]
+  let arry=Decrescente
+
+  const maiorDaArray=(numArray)=> {
+      return Math.max.apply(null, numArray);
+  }
+
+  const menorDaArray=(numArray)=> {
+      return Math.min.apply(null, numArray);
+  }
+
+  for(let i=0;i<musicasTocadas.length; i++){
+      Decrescente[i]=maiorDaArray(arr)
+      arr.splice(arr.indexOf(maiorDaArray(arr)),1,-1)
+  }
+
+  arr.forEach((element,index)=>{
+      
+      if(index %2!==0){
+          embaralhado[index]=menorDaArray(arry)
+          arry.splice(arry.indexOf(menorDaArray(arry)),1)
+          console.log(arry)
+          
+      }else{
+          embaralhado[index]=maiorDaArray(arry)
+          arry.splice(arry.indexOf(maiorDaArray(arry)),1)
+          console.log(arry)
+      }
+      
+  })
+  
+  return embaralhado
+   
+}
